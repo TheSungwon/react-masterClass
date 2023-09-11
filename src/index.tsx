f1f1be958e3d66bcdfeb8f78f4e41aab69199210
sungwon <thesungwon@gmail.com>
2023-09-11 22:25:59 +0900

    ~3.6

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 //import ReactDOM from "react-dom/client";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme";
 //const root = ReactDOM.createRoot(document.getElementById("root"));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
