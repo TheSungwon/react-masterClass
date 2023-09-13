4c6dbd26b0e5098fe653f45ff0fa3f71d9e9b41a
sungwon <thesungwon@gmail.com>
2023-09-13 22:47:09 +0900

    ~3.7

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,31 @@ function App() {
     color: ${(props) => props.theme.textColor};
   `;
 
+  interface DummyProps {
+    text: string;
+    otherThing?: boolean; // optional
+    active?: boolean;
+  }
+
+  function Dummy({ text, otherThing, active = false }: DummyProps) {
+    return <H1>{text}</H1>;
+  }
+
+  //https://legacy.reactjs.org/docs/events.html
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event);
+  };
+
+  // typescript library 가 없는 라이브러리면
+  // npm i --save-dev @types/~~~ , ~~~라이브러리 이름
   return (
     <Container>
       <H1>protected</H1>
+      <Dummy text="dddd" active />
+      <Dummy text="dddd" active={true} />
+      {/* <form> */}
+      <button onClick={onClick}></button>
+      {/* </form> */}
     </Container>
   );
 }
