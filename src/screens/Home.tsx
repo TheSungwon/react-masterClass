4d25a9afa75d2cfd0436a2490321d55bd0c47343
sungwon <thesungwon@gmail.com>
2023-09-19 23:35:37 +0900

    ~4.3

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>home</h1>;
+  const users: any = []; //에러발생을 위해 선언
+
+  return <h1>{users[0].name}</h1>; //에러발생을 위해 선언
 }
 
 export default Home;
--- src/screens/Router.tsx
+++ src/screens/Router.tsx
@@ -8,18 +8,21 @@ import Header from "../components/Header";
 import Home from "./Home";
 import About from "./About";
 import Root from "../Root";
+import NotFound from "./NotFound";
+import ErrorComponent from "../components/ErrorComponent";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
+      { path: "", element: <Home />, errorElement: <ErrorComponent /> },
       {
         path: "about",
         element: <About />,
       },
-      { path: "", element: <Home /> },
     ],
+    errorElement: <NotFound />,
   },
 ]);
 
