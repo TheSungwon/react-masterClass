fb2ba5a59212e07aefe4716c3dbad67e34b1ea57
sungwon <thesungwon@gmail.com>
2023-10-03 01:01:56 +0900

    ~5.3

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,16 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
   const Title = styled.h1`
@@ -7,7 +18,11 @@ function Coins() {
     font-size: 48px;
   `;
 
-  const Container = styled.div``;
+  const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+  `;
 
   const Header = styled.header`
     height: 10vh;
@@ -16,9 +31,7 @@ function Coins() {
     align-items: center;
   `;
 
-  const CoinsList = styled.ul`
-    padding: 0px 20px;
-  `;
+  const CoinsList = styled.ul``;
 
   const Coin = styled.li`
     background-color: white;
@@ -39,49 +52,49 @@ function Coins() {
     }
   `;
 
-  const coins = [
-    {
-      id: "btc-bitcoin",
-      name: "Bitcoin",
-      symbol: "BTC",
-      rank: 1,
-      is_new: false,
-      is_active: true,
-      type: "coin",
-    },
-    {
-      id: "eth-ethereum",
-      name: "Ethereum",
-      symbol: "ETH",
-      rank: 2,
-      is_new: false,
-      is_active: true,
-      type: "coin",
-    },
-    {
-      id: "hex-hex",
-      name: "HEX",
-      symbol: "HEX",
-      rank: 3,
-      is_new: false,
-      is_active: true,
-      type: "token",
-    },
-  ];
+  const breathe = keyframes`
+  0% {opacity:1;}
+  50%{opacity:0.5;}
+  100%{opacity:1;}
+  `;
+  const Loader = styled.span`
+    color: red;
+    text-align: center;
+    animation: ${breathe} 2s infinite;
+    display: block;
+  `;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   (() => console.log(1))();
+  // },[]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 99));
+      //console.log(json.slice(0, 99));
+      setLoading(false);
+    })();
+  }, []);
 
+  console.log(coins);
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.name}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader> LOADING . . . </Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.name}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 }
