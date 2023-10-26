7eb7d1ae4aed40c3ea6454ca32062d995e4b7b7a
sungwon <thesungwon@gmail.com>
2023-10-26 23:38:44 +0900

    ~6.8

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -28,9 +28,25 @@ import { useForm } from "react-hook-form";
 //     </div>
 //   );
 // }
+interface IForm {
+  email: string;
+  firstName?: string; //필수값 아니라면 ?
+  lastName?: string;
+  password: string;
+  passwordCheck: string;
+}
 
 function ToDoList() {
-  const { register, watch, handleSubmit, formState } = useForm();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com", //defaultValues로 기본값 설정
+    },
+  });
   //   console.log(register("aaaa"));
   //register가 위의 것들을 해줌 onChange, setState..
   //register반환 값 .. name, onBlur, onChange, ref ..
@@ -45,14 +61,15 @@ function ToDoList() {
 
   //handleSubmit은 validation, preventDefault, ...기능
   //onValid 는필수, 와 onInvalid를 인자로 받음 onValid는 유효할때, onValid는 X
-  const onValid = (data: any) => {
+  const onValid = (data: IForm) => {
     console.log(data, "onValid");
   };
   //...register 에 {required:true} 유효성검증 , {required:true, minLength: 5}
   //입력안된 채로 submit하면 invalid input에 포커스 커서
 
   //formState
-  console.log(formState.errors); //어떤 종류의 에러인지, 에러가 발생했는지
+  //console.log(formState.errors); //어떤 종류의 에러인지, 에러가 발생했는지
+  console.log(errors);
 
   return (
     <div>
@@ -62,22 +79,50 @@ function ToDoList() {
           flexDirection: "column",
           width: "300px",
         }}
-        onSubmit={handleSubmit(onValid)}
+        onSubmit={handleSubmit(onValid)} //handleSubmit => hook form
       >
         <input
-          {...(register("todo"), { required: true, minLength: 5 })}
-          placeholder="Write a To Do"
+          {...register("email", {
+            required: "email check error",
+            pattern: {
+              value: /^[A-Za-z0-9._%+_]+@naver.com/,
+              message: "only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
         />
+        <span style={{ color: "red" }}>{errors?.email?.message as string}</span>
         {/* <input {...(register("qq"), { required: true })} placeholder="qq" /> 
         괄호 위치 확인!!!!!!! 
          괄호가 (register required true면 submit할 때 input box에 toggle 생성? + submit, formState안됨
          괄호가 register( required true면 submit할 때 input box에 toggle 생성 X, formState됨
         */}
-        <input {...register("qq", { required: true })} placeholder="qq" />
         <input
-          {...register("ww", { required: "has errors", minLength: 10 })}
-          placeholder="ww"
+          {...register("firstName", {
+            required: "write here",
+            minLength: {
+              value: 5,
+              message: "write more than five words",
+            },
+          })}
+          placeholder="firstName"
         />
+        <span style={{ color: "red" }}>
+          {errors?.firstName?.message as string}
+        </span>
+        <input
+          {...register("lastName", {
+            required: "has errors",
+            minLength: {
+              value: 5,
+              message: "write more than five words",
+            },
+          })}
+          placeholder="lastName"
+        />
+        <span style={{ color: "red" }}>
+          {errors?.lastName?.message as string}
+        </span>
         <input
           {...register("password", {
             required: "password errors",
@@ -88,6 +133,23 @@ function ToDoList() {
           })}
           placeholder="password"
         />
+        <span style={{ color: "red" }}>
+          {errors?.password?.message as string}
+        </span>
+        <input
+          {...register("passwordCheck", {
+            required: "password errors",
+            minLength: {
+              value: 5,
+              message: "5글자이상 필요",
+            },
+          })}
+          placeholder="password check"
+        />
+        <span style={{ color: "red" }}>
+          {errors?.passwordCheck?.message as string}
+        </span>
+
         <button>Add</button>
       </form>
     </div>
