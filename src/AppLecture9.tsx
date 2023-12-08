1ed6ee1d1840c149b224a3392b22f1f85a99fb56
sungwon <thesungwon@gmail.com>
2023-12-09 00:58:46 +0900

    ~9.1

--- src/AppLecture9.tsx
+++ src/AppLecture9.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routesLecture9/Home";
+import Tv from "./routesLecture9/Tv";
+import Search from "./routesLecture9/Search";
+import Header from "./componentsLecture9/Heaer";
+
 function AppLecture9() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppLecture9;
+
+// router 6이전 버전
+// <Router>
+//     <Switch>
+//         <Route>
+//         </Route>
+//     </Switch>
+// </Router>
