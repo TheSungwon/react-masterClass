95b6d1f5c7fa6e11e01f40511cd91329d59174c8
sungwon <thesungwon@gmail.com>
2023-12-14 04:04:51 +0900

    ~9.6

--- src/apiLecture9.tsx
+++ src/apiLecture9.tsx
@@ -1,6 +1,24 @@
 const API_KEY = "dc921e41cb528b0abd015353b9f36f29";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+//getMovies의 Typescript
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  maximum: string;
+  minimum: string;
+  page: number;
+  total_pages: number;
+  total_results: number;
+  results: IMovie[];
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (result) => result.json()
--- src/routesLecture9/Home.tsx
+++ src/routesLecture9/Home.tsx
@@ -1,10 +1,65 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../apiLectre9";
+import { IGetMoviesResult, getMovies } from "../apiLecture9";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { makeImagePath } from "../utils";
+
+const Wrapper = styled(motion.div)`
+  background-color: black;
+`;
+
+const Loader = styled(motion.div)`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  /* background-image: ${(props) => props.bgPhoto};  url()안에 넣어줘야 함*/
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  //위에서 아래로 gradient // 0, 0, 0, 1 -> 0, 0, 0, 0
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery(["aa"], getMovies);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
   console.log(data);
-  return <div style={{ backgroundColor: "white", height: "200vh" }}>home</div>;
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            {/* data?.results[0].backdrop_path 가 없으면 ""을 보내게 함 */}
+
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
